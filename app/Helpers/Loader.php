f4658d994998cec8e0737b74f2bf84d9a0abb389
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2022-05-02 16:24:06 +0700

    add loader javasvascript helper

---
+++ app/Helpers/Loader.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Support\Facades\Blade;
+use MatthiasMullie\Minify\JS;
+
+class Loader
+{
+    public static function render_js(string $path, array $data = []): string
+    {
+        $full_path = resource_path("js/views/$path");
+        if (file_exists($full_path)) {
+            $minifier = new JS($full_path);
+            $result = Blade::render($minifier->minify(), $data);
+            return $result;
+        } else {
+            return "console.log('javascript {$path} not found')";
+        }
+    }
+}
