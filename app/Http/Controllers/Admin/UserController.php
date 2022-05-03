bcdb79d74a9de3af5c6114531b5205d9d758a9f1
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2022-05-04 05:29:02 +0700

    user controller add repository

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -4,52 +4,26 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Repository\Admin\UserRepository;
+use Exception;
 use Illuminate\Http\Request;
 use Yajra\Datatables\Datatables;
 use Laravel\Fortify\Rules\Password;
 use Illuminate\Support\Facades\Hash;
 use League\Config\Exception\ValidationException;
-use League\Flysystem\Exception;
 
 class UserController extends Controller
 {
-    private $query = [];
+    private $repository;
+    public function __construct(UserRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     public function index(Request $request)
     {
         if (request()->ajax()) {
-            $year = (int)date('Y');
-            $year_add_one = $year + 1;
-            $this->query['birthday_countdown'] = <<<SQL
-                ( if( DATEDIFF(date(concat('{$year}-', month(date_of_birth), '-', day(date_of_birth))), CURDATE()) < 0,
-                    DATEDIFF(date(concat('{$year_add_one}-', month(date_of_birth), '-', day(date_of_birth))), CURDATE()) ,
-                    DATEDIFF(date(concat('{$year}-', month(date_of_birth), '-', day(date_of_birth))), CURDATE()) )
-                )
-            SQL;
-            $this->query['birthday_countdown_alias'] = 'birthday_countdown';
-
-            $user = User::select(['id', 'name', 'role', 'email', 'active', 'profile_photo_path', 'date_of_birth', 'angkatan'])
-                ->selectRaw('IF(active = 1, "Yes", "No") as active_str')
-                ->selectRaw("{$this->query['birthday_countdown']} as {$this->query['birthday_countdown_alias']}");
-            // filter
-            if (isset($request->filter)) {
-                $filter = $request->filter;
-                if ($filter['active'] != '') {
-                    $user->where('active', '=', $filter['active']);
-                }
-                if ($filter['role'] != '') {
-                    $user->where('role',  '=', $filter['role']);
-                }
-            }
-
-            return Datatables::of($user)
-                ->addIndexColumn()
-                ->addColumn('role_str', function (User $user) {
-                    return ucfirst(implode(' ', explode('_', $user->role)));
-                })
-                ->filterColumn($this->query['birthday_countdown_alias'], function ($query, $keyword) {
-                    $query->whereRaw("{$this->query['birthday_countdown']} like '%$keyword%'");
-                })
-                ->make(true);
+            return $this->repository->datatable($request);
         }
 
         $page_attr = [
@@ -65,94 +39,23 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
-
-        try {
-            $user_role = implode(",", User::getAllRole());
-            $request->validate([
-                'name' => ['required', 'string', 'max:255'],
-                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-                'role' => ['required', 'string', 'in:' . $user_role],
-                'date_of_birth' => ['required', 'date'],
-                'angkatan' => ['required', 'int'],
-                'active' => ['required', 'int', 'in:1,0'],
-                'password' => ['required', 'string', new Password]
-            ]);
-
-            User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'date_of_birth' => $request->date_of_birth,
-                'angkatan' => $request->angkatan,
-                'role' => $request->role,
-                'active' => $request->active,
-                'password' => Hash::make($request->password),
-                // // 'created_by' => auth()->user()->id,
-            ]);
-            return response()->json();
-        } catch (ValidationException $error) {
-            return response()->json([
-                'message' => 'Something went wrong',
-                'error' => $error,
-            ], 500);
-        }
+        return $this->repository->store($request);
     }
 
     public function update(Request $request)
     {
-        try {
-            $user_role = implode(",", User::getAllRole());
-            $user = User::find($request->id);
-            $request->validate([
-                'id' => ['required', 'int'],
-                'name' => ['required', 'string', 'max:255'],
-                'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,' . $user->id],
-                'role' => ['required', 'string', 'in:' . $user_role],
-                'date_of_birth' => ['required', 'date'],
-                'angkatan' => ['required', 'int'],
-                'active' => ['required', 'int', 'in:1,0'],
-                'password' => $request->password ? ['required', 'string', new Password] : ''
-            ]);
-
-            if ($request->password) {
-                $user->password = Hash::make($request->password);
-            }
-
-            $user->name = $request->name;
-            $user->email = $request->email;
-            $user->role = $request->role;
-            $user->date_of_birth = $request->date_of_birth;
-            $user->angkatan = $request->angkatan;
-            $user->active = $request->active;
-            // $model->updated_by = auth()->user()->id;
-
-            $user->save();
-            return response()->json();
-        } catch (ValidationException $error) {
-            return response()->json([
-                'message' => 'Something went wrong',
-                'error' => $error,
-            ], 500);
-        }
+        return $this->repository->update($request);
     }
 
     public function delete(Request $user)
     {
-        try {
-            $user = User::find($user->id);
-            $user->delete();
-            return response()->json();
-        } catch (ValidationException $error) {
-            return response()->json([
-                'message' => 'Something went wrong',
-                'error' => $error,
-            ], 500);
-        }
+        return $this->repository->delete($user);
     }
 
     public function change_password(Request $request)
     {
         $page_attr = [
-            'title' => 'View User',
+            'title' => 'Ganti Password',
             'breadcrumbs' => [
                 ['name' => 'Dashboard'],
             ],
@@ -163,24 +66,6 @@ class UserController extends Controller
 
     public function save_password(Request $request)
     {
-        try {
-            $request->validate([
-                'current_password' => ['required', 'string', 'max:255'],
-                'new_password' => ['required', 'string', new Password]
-            ]);
-            $user = User::find(auth()->user()->id);
-            if (Hash::check($request->current_password, $user->password)) {
-                $user->password = Hash::make($request->new_password);
-                $user->save();
-            } else {
-                throw new Exception("Password Lama Salah. Jika anda lupa silahkan hubungi administrator.");
-            }
-            return response()->json();
-        } catch (ValidationException $error) {
-            return response()->json([
-                'message' => 'Something went wrong',
-                'error' => $error,
-            ], 500);
-        }
+        return $this->repository->save_password($request);
     }
 }
---
+++ app/Repository/Admin/UserRepository.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Repository\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Yajra\Datatables\Datatables;
+use Laravel\Fortify\Rules\Password;
+use Illuminate\Support\Facades\Hash;
+use League\Config\Exception\ValidationException;
+
+class UserRepository
+{
+    private $query = [];
+    public function datatable(Request $request)
+    {
+        $year = (int)date('Y');
+        $year_add_one = $year + 1;
+        $this->query['birthday_countdown'] = <<<SQL
+            ( if( DATEDIFF(date(concat('{$year}-', month(date_of_birth), '-', day(date_of_birth))), CURDATE()) < 0,
+                DATEDIFF(date(concat('{$year_add_one}-', month(date_of_birth), '-', day(date_of_birth))), CURDATE()) ,
+                DATEDIFF(date(concat('{$year}-', month(date_of_birth), '-', day(date_of_birth))), CURDATE()) )
+            )
+        SQL;
+        $this->query['birthday_countdown_alias'] = 'birthday_countdown';
+
+        $user = User::select(['id', 'name', 'role', 'email', 'active', 'profile_photo_path', 'date_of_birth', 'angkatan'])
+            ->selectRaw('IF(active = 1, "Yes", "No") as active_str')
+            ->selectRaw("{$this->query['birthday_countdown']} as {$this->query['birthday_countdown_alias']}");
+        // filter
+        if (isset($request->filter)) {
+            $filter = $request->filter;
+            if ($filter['active'] != '') {
+                $user->where('active', '=', $filter['active']);
+            }
+            if ($filter['role'] != '') {
+                $user->where('role',  '=', $filter['role']);
+            }
+        }
+
+        return Datatables::of($user)
+            ->addIndexColumn()
+            ->addColumn('role_str', function (User $user) {
+                return ucfirst(implode(' ', explode('_', $user->role)));
+            })
+            ->filterColumn($this->query['birthday_countdown_alias'], function ($query, $keyword) {
+                $query->whereRaw("{$this->query['birthday_countdown']} like '%$keyword%'");
+            })
+            ->make(true);
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $user_role = implode(",", User::getAllRole());
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'role' => ['required', 'string', 'in:' . $user_role],
+                'date_of_birth' => ['required', 'date'],
+                'angkatan' => ['required', 'int'],
+                'active' => ['required', 'int', 'in:1,0'],
+                'password' => ['required', 'string', new Password]
+            ]);
+
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'date_of_birth' => $request->date_of_birth,
+                'angkatan' => $request->angkatan,
+                'role' => $request->role,
+                'active' => $request->active,
+                'password' => Hash::make($request->password),
+                // // 'created_by' => auth()->user()->id,
+            ]);
+            return response()->json();
+        } catch (ValidationException $error) {
+            return response()->json([
+                'message' => 'Something went wrong',
+                'error' => $error,
+            ], 500);
+        }
+    }
+
+    public function update(Request $request)
+    {
+        try {
+            $user_role = implode(",", User::getAllRole());
+            $user = User::find($request->id);
+            $request->validate([
+                'id' => ['required', 'int'],
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,' . $user->id],
+                'role' => ['required', 'string', 'in:' . $user_role],
+                'date_of_birth' => ['required', 'date'],
+                'angkatan' => ['required', 'int'],
+                'active' => ['required', 'int', 'in:1,0'],
+                'password' => $request->password ? ['required', 'string', new Password] : ''
+            ]);
+
+            if ($request->password) {
+                $user->password = Hash::make($request->password);
+            }
+
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->role = $request->role;
+            $user->date_of_birth = $request->date_of_birth;
+            $user->angkatan = $request->angkatan;
+            $user->active = $request->active;
+            // $model->updated_by = auth()->user()->id;
+
+            $user->save();
+            return response()->json();
+        } catch (ValidationException $error) {
+            return response()->json([
+                'message' => 'Something went wrong',
+                'error' => $error,
+            ], 500);
+        }
+    }
+
+    public function delete(Request $user)
+    {
+        try {
+            $user = User::find($user->id);
+            $user->delete();
+            return response()->json();
+        } catch (ValidationException $error) {
+            return response()->json([
+                'message' => 'Something went wrong',
+                'error' => $error,
+            ], 500);
+        }
+    }
+
+    public function save_password(Request $request)
+    {
+        try {
+            $request->validate([
+                'current_password' => ['required', 'string', 'max:255'],
+                'new_password' => ['required', 'string', new Password]
+            ]);
+            $user = User::find(auth()->user()->id);
+            if (Hash::check($request->current_password, $user->password)) {
+                $user->password = Hash::make($request->new_password);
+                $user->save();
+            } else {
+                throw new Exception("Password Lama Salah. Jika anda lupa silahkan hubungi administrator.");
+            }
+            return response()->json();
+        } catch (ValidationException $error) {
+            return response()->json([
+                'message' => 'Something went wrong',
+                'error' => $error,
+            ], 500);
+        }
+    }
+}
