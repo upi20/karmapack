b0265204a12b869116aced562479428814ac92bf
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2022-08-06 00:43:35 +0700

    implement menu to panel admin

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -54,3 +54,62 @@ if (!function_exists('menu')) {
         return menu_parse($menu);
     }
 }
+
+if (!function_exists('sidebar_menu_admin')) {
+    /**
+     * Helpers for build menu admin sidebar admin.
+     *
+     * @return array
+     */
+    function sidebar_menu_admin()
+    {
+        $menus = menu(auth()->user()->id);
+        $menu_body = '';
+        foreach ($menus as $m) {
+            $menu = (object)$m;
+
+            // 1 check separator
+            $separator = $menu->type == 0;
+
+            // active
+            $active_class = $menu->active ? 'active' : '';
+
+            if ($separator) {
+                // separator
+                $menu_body .= "<li class=\"sub-category\"><h3>{$menu->title}</h3></li> ";
+            } elseif ($menu->children) {
+                $child_menu = '';
+                foreach ($menu->children as $c) {
+                    $child = (object)$c;
+                    $active = $child->active ? 'active' : '';
+                    $child_menu .= "<li><a href=\"$child->url\" class=\"slide-item $active\">$child->title</a></li>";
+                }
+
+                $active_1 = $menu->active ? 'is-expanded' : '';
+                $active_2 = $menu->active ? 'active' : '';
+                $menu_body .= " <li class=\"slide $active_1\">
+                                    <a class=\"side-menu__item $active_1 $active_2\" data-bs-toggle=\"slide\" href=\"javascript:void(0)\">
+                                        <i class=\"side-menu__icon $menu->icon\"></i>
+                                        <span class=\"side-menu__label\">$menu->title</span>
+                                        <i class=\"angle fe fe-chevron-right\"></i>
+                                    </a>
+                                    <ul class=\"slide-menu\">
+                                        $child_menu
+                                    </ul>
+                                </li> ";
+            } else {
+                $menu_body .= "<li class=\"slide\">
+                        <a class=\"side-menu__item $active_class\" data-bs-toggle=\"slide\" href=\"$menu->url\">
+                            <i class=\"side-menu__icon $menu->icon\"></i>
+                            <span class=\"side-menu__label\">$menu->title</span>
+                        </a>
+                    </li>";
+            }
+        }
+
+        // head element
+        $menu_head = '<ul class="side-menu">';
+        $menu_footer = '</ul>';
+        return $menu_head . $menu_body . $menu_footer;
+    }
+}
--- resources/views/templates/admin/body/sidebar.blade.php
+++ resources/views/templates/admin/body/sidebar.blade.php
@@ -1,11 +1,3 @@
-<?php
-$master_helper = new \App\Helpers\Frontend\Template\Master();
-$menu_extend = array_merge($master_helper->menuBidang(), $master_helper->menuPendaftaran());
-$menu = new \App\Helpers\Admin\Template\SidebarMenu(app('config')->get('menu_list')[auth()->user()->role], $page_attr_navigation, $menu_extend);
-
-?>
-
-
 <!--APP-SIDEBAR-->
 <div class="sticky">
     <div class="app-sidebar__overlay" data-bs-toggle="sidebar"></div>
@@ -24,16 +16,19 @@ $menu = new \App\Helpers\Admin\Template\SidebarMenu(app('config')->get('menu_lis
             <!-- LOGO -->
         </div>
         <div class="main-sidemenu">
-            <div class="slide-left disabled" id="slide-left"><svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191"
-                    width="24" height="24" viewBox="0 0 24 24">
+            <div class="slide-left disabled" id="slide-left">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24"
+                    viewBox="0 0 24 24">
                     <path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z" />
-                </svg></div>
-            {!! $menu->generate() !!}
-            <div class="slide-right" id="slide-right"><svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191"
-                    width="24" height="24" viewBox="0 0 24 24">
+                </svg>
+            </div>
+            {!! sidebar_menu_admin() !!}
+            <div class="slide-right" id="slide-right">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24"
+                    viewBox="0 0 24 24">
                     <path d="M10.707 17.707 16.414 12l-5.707-5.707-1.414 1.414L13.586 12l-4.293 4.293z" />
-                </svg></div>
+                </svg>
+            </div>
         </div>
     </div>
-    <!--/APP-SIDEBAR-->
 </div>
