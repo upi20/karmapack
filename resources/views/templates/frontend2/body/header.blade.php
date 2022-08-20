847e7b5c1d505d0d94c057452d020b096e878c6f
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2022-08-20 19:31:44 +0700

    delete resource not use

--- resources/views/templates/frontend2/body/header.blade.php
+++ resources/views/templates/frontend2/body/header.blade.php
@@ -119,102 +119,14 @@
 
                                                     <button
                                                         class="d-flex items-center justify-center size-40 rounded-full bg-purple-3"
-                                                        data-el-toggle=".js-search-toggle">
-                                                        <img src="{{ asset('assets/templates/frontend2/img/menus/close.svg') }}"
-                                                            alt="icon">
+                                                        data-el-toggle=".js-search-toggle" type="button">
+                                                        <div class="icon-close text-dark-1 text-16"></div>
                                                     </button>
                                                 </form>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="header-search__bg" data-el-toggle=".js-search-toggle">
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="toggle-element js-cart-toggle">
-                                <div class="header-cart bg-white -dark-bg-dark-1 rounded-8">
-                                    <div class="px-30 pt-30 pb-10">
-
-                                        <div class="row justify-between x-gap-40 pb-20">
-                                            <div class="col">
-                                                <div class="row x-gap-10 y-gap-10">
-                                                    <div class="col-auto">
-                                                        <img src="{{ asset('assets/templates/frontend2/img/menus/cart/1.png') }}"
-                                                            alt="image">
-                                                    </div>
-
-                                                    <div class="col">
-                                                        <div class="text-dark-1 lh-15">The Ultimate Drawing
-                                                            Course Beginner to Advanced...
-                                                        </div>
-
-                                                        <div class="d-flex items-center mt-10">
-                                                            <div class="lh-12 fw-500 line-through text-light-1 mr-10">
-                                                                $179</div>
-                                                            <div class="text-18 lh-12 fw-500 text-dark-1">
-                                                                $79</div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div class="col-auto">
-                                                <button><img
-                                                        src="{{ asset('assets/templates/frontend2/img/menus/close.svg') }}"
-                                                        alt="icon"></button>
-                                            </div>
-                                        </div>
-
-                                        <div class="row justify-between x-gap-40 pb-20">
-                                            <div class="col">
-                                                <div class="row x-gap-10 y-gap-10">
-                                                    <div class="col-auto">
-                                                        <img src="{{ asset('assets/templates/frontend2/img/menus/cart/2.png') }}"
-                                                            alt="image">
-                                                    </div>
-
-                                                    <div class="col">
-                                                        <div class="text-dark-1 lh-15">User Experience
-                                                            Design Essentials - Adobe XD UI UX...
-                                                        </div>
-
-                                                        <div class="d-flex items-center mt-10">
-                                                            <div class="lh-12 fw-500 line-through text-light-1 mr-10">
-                                                                $179</div>
-                                                            <div class="text-18 lh-12 fw-500 text-dark-1">
-                                                                $79</div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div class="col-auto">
-                                                <button><img
-                                                        src="{{ asset('assets/templates/frontend2/img/menus/close.svg') }}"
-                                                        alt="icon"></button>
-                                            </div>
-                                        </div>
-
-                                    </div>
-
-                                    <div class="px-30 pt-20 pb-30 border-top-light">
-                                        <div class="d-flex justify-between">
-                                            <div class="text-18 lh-12 text-dark-1 fw-500">Total:</div>
-                                            <div class="text-18 lh-12 text-dark-1 fw-500">$659</div>
-                                        </div>
-
-                                        <div class="row x-gap-20 y-gap-10 pt-30">
-                                            <div class="col-sm-6">
-                                                <button
-                                                    class="button py-20 -dark-1 text-white -dark-button-white col-12">View
-                                                    Cart</button>
-                                            </div>
-                                            <div class="col-sm-6">
-                                                <button
-                                                    class="button py-20 -purple-1 text-white col-12">Checkout</button>
-                                            </div>
-                                        </div>
                                     </div>
                                 </div>
                             </div>
