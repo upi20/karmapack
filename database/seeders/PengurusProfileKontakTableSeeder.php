530dda25156954a97779928dbef916671b938613
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2022-08-31 22:09:45 +0700

    backup 2022-08-31

--- database/seeders/PengurusProfileKontakTableSeeder.php
+++ database/seeders/PengurusProfileKontakTableSeeder.php
@@ -21,93 +21,264 @@ class PengurusProfileKontakTableSeeder extends Seeder
         \DB::table('pengurus_profile_kontak')->insert(array (
             0 => 
             array (
-                'id' => 7,
-                'kontak_tipe_id' => 1,
-                'user_id' => 1,
+                'id' => '7',
+                'kontak_tipe_id' => '1',
+                'user_id' => '1',
                 'value' => 'https://facebook.com/iseplutpinur7/',
                 'created_at' => '2022-04-23 07:05:43',
                 'updated_at' => '2022-04-23 07:05:43',
             ),
             1 => 
             array (
-                'id' => 8,
-                'kontak_tipe_id' => 5,
-                'user_id' => 1,
+                'id' => '8',
+                'kontak_tipe_id' => '5',
+                'user_id' => '1',
                 'value' => 'https://www.instagram.com/iseplutpinur/',
                 'created_at' => '2022-04-23 07:06:21',
                 'updated_at' => '2022-04-23 07:06:21',
             ),
             2 => 
             array (
-                'id' => 9,
-                'kontak_tipe_id' => 5,
-                'user_id' => 17,
+                'id' => '9',
+                'kontak_tipe_id' => '5',
+                'user_id' => '17',
                 'value' => '@hasnhaaa_nita',
                 'created_at' => '2022-07-15 22:16:33',
                 'updated_at' => '2022-07-15 22:16:33',
             ),
             3 => 
             array (
-                'id' => 10,
-                'kontak_tipe_id' => 5,
-                'user_id' => 101,
+                'id' => '10',
+                'kontak_tipe_id' => '5',
+                'user_id' => '101',
                 'value' => 'https://instagram.com/widiimaulidiaa_?igshid=YmMyMTA2M2Y=',
                 'created_at' => '2022-07-15 22:29:08',
                 'updated_at' => '2022-07-15 22:29:08',
             ),
             4 => 
             array (
-                'id' => 11,
-                'kontak_tipe_id' => 1,
-                'user_id' => 21,
+                'id' => '11',
+                'kontak_tipe_id' => '1',
+                'user_id' => '21',
                 'value' => 'https://m.facebook.com/profile.php',
                 'created_at' => '2022-07-15 22:40:12',
                 'updated_at' => '2022-07-15 22:40:12',
             ),
             5 => 
             array (
-                'id' => 12,
-                'kontak_tipe_id' => 4,
-                'user_id' => 99,
+                'id' => '12',
+                'kontak_tipe_id' => '4',
+                'user_id' => '99',
                 'value' => 'Ahmad Dimyati Ridwan',
                 'created_at' => '2022-07-16 11:07:39',
                 'updated_at' => '2022-07-16 11:16:50',
             ),
             6 => 
             array (
-                'id' => 13,
-                'kontak_tipe_id' => 1,
-                'user_id' => 99,
+                'id' => '13',
+                'kontak_tipe_id' => '1',
+                'user_id' => '99',
                 'value' => 'Ahmad Dimyati Ridwan',
                 'created_at' => '2022-07-16 11:16:28',
                 'updated_at' => '2022-07-16 11:16:28',
             ),
             7 => 
             array (
-                'id' => 14,
-                'kontak_tipe_id' => 2,
-                'user_id' => 99,
+                'id' => '14',
+                'kontak_tipe_id' => '2',
+                'user_id' => '99',
                 'value' => 'Ahmad Dimyati Ridwan',
                 'created_at' => '2022-07-16 11:17:42',
                 'updated_at' => '2022-07-16 11:17:42',
             ),
             8 => 
             array (
-                'id' => 15,
-                'kontak_tipe_id' => 3,
-                'user_id' => 99,
+                'id' => '15',
+                'kontak_tipe_id' => '3',
+                'user_id' => '99',
                 'value' => 'Ahmad Dimyati Ridwan',
                 'created_at' => '2022-07-16 11:18:12',
                 'updated_at' => '2022-07-16 11:18:12',
             ),
             9 => 
             array (
-                'id' => 16,
-                'kontak_tipe_id' => 5,
-                'user_id' => 99,
+                'id' => '16',
+                'kontak_tipe_id' => '5',
+                'user_id' => '99',
                 'value' => 'ahmaddimyatiridwan16',
                 'created_at' => '2022-07-16 11:19:16',
                 'updated_at' => '2022-07-16 11:19:16',
+            ),
+            10 => 
+            array (
+                'id' => '17',
+                'kontak_tipe_id' => '5',
+                'user_id' => '57',
+                'value' => 'Nu\'man Basir',
+                'created_at' => '2022-08-30 10:27:56',
+                'updated_at' => '2022-08-30 10:27:56',
+            ),
+            11 => 
+            array (
+                'id' => '18',
+                'kontak_tipe_id' => '5',
+                'user_id' => '75',
+                'value' => '@sptiaran11',
+                'created_at' => '2022-08-30 17:40:16',
+                'updated_at' => '2022-08-30 17:40:16',
+            ),
+            12 => 
+            array (
+                'id' => '19',
+                'kontak_tipe_id' => '5',
+                'user_id' => '87',
+                'value' => '@alvirdadmyti521_',
+                'created_at' => '2022-08-30 17:49:23',
+                'updated_at' => '2022-08-30 17:49:23',
+            ),
+            13 => 
+            array (
+                'id' => '20',
+                'kontak_tipe_id' => '1',
+                'user_id' => '87',
+                'value' => 'Alvirda Damayanti A',
+                'created_at' => '2022-08-30 17:49:39',
+                'updated_at' => '2022-08-30 17:49:39',
+            ),
+            14 => 
+            array (
+                'id' => '21',
+                'kontak_tipe_id' => '3',
+                'user_id' => '87',
+                'value' => '085861610741',
+                'created_at' => '2022-08-30 17:49:54',
+                'updated_at' => '2022-08-30 17:49:54',
+            ),
+            15 => 
+            array (
+                'id' => '22',
+                'kontak_tipe_id' => '3',
+                'user_id' => '97',
+                'value' => '085861444715',
+                'created_at' => '2022-08-30 17:57:50',
+                'updated_at' => '2022-08-30 17:57:50',
+            ),
+            16 => 
+            array (
+                'id' => '23',
+                'kontak_tipe_id' => '5',
+                'user_id' => '85',
+                'value' => 'Muhamadraihanrahman_23',
+                'created_at' => '2022-08-30 18:02:04',
+                'updated_at' => '2022-08-30 18:02:04',
+            ),
+            17 => 
+            array (
+                'id' => '24',
+                'kontak_tipe_id' => '5',
+                'user_id' => '76',
+                'value' => 'snurjamilaa02',
+                'created_at' => '2022-08-30 18:21:32',
+                'updated_at' => '2022-08-30 18:21:32',
+            ),
+            18 => 
+            array (
+                'id' => '25',
+                'kontak_tipe_id' => '5',
+                'user_id' => '84',
+                'value' => 'indraskmra',
+                'created_at' => '2022-08-30 20:18:55',
+                'updated_at' => '2022-08-30 20:18:55',
+            ),
+            19 => 
+            array (
+                'id' => '26',
+                'kontak_tipe_id' => '5',
+                'user_id' => '73',
+                'value' => 'Deriis19',
+                'created_at' => '2022-08-30 20:33:08',
+                'updated_at' => '2022-08-30 20:33:08',
+            ),
+            20 => 
+            array (
+                'id' => '27',
+                'kontak_tipe_id' => '5',
+                'user_id' => '96',
+                'value' => 'nasriatulhamidah',
+                'created_at' => '2022-08-31 08:29:21',
+                'updated_at' => '2022-08-31 08:29:21',
+            ),
+            21 => 
+            array (
+                'id' => '28',
+                'kontak_tipe_id' => '4',
+                'user_id' => '74',
+                'value' => 'https://www.instagram.com/feery.id/',
+                'created_at' => '2022-08-31 11:16:00',
+                'updated_at' => '2022-08-31 11:16:00',
+            ),
+            22 => 
+            array (
+                'id' => '29',
+                'kontak_tipe_id' => '1',
+                'user_id' => '74',
+                'value' => 'https://www.youtube.com/channel/UC7eJqjdsRePej9HFcj3ytwA',
+                'created_at' => '2022-08-31 11:16:32',
+                'updated_at' => '2022-08-31 11:16:32',
+            ),
+            23 => 
+            array (
+                'id' => '30',
+                'kontak_tipe_id' => '2',
+                'user_id' => '74',
+                'value' => 'https://twitter.com/Ferry020103',
+                'created_at' => '2022-08-31 11:17:14',
+                'updated_at' => '2022-08-31 11:17:14',
+            ),
+            24 => 
+            array (
+                'id' => '31',
+                'kontak_tipe_id' => '1',
+                'user_id' => '74',
+                'value' => 'https://www.facebook.com/profile.php?id=100054959199214',
+                'created_at' => '2022-08-31 11:18:41',
+                'updated_at' => '2022-08-31 11:18:41',
+            ),
+            25 => 
+            array (
+                'id' => '32',
+                'kontak_tipe_id' => '3',
+                'user_id' => '74',
+                'value' => 'https://wa.me/6285872245293',
+                'created_at' => '2022-08-31 11:20:16',
+                'updated_at' => '2022-08-31 11:20:16',
+            ),
+            26 => 
+            array (
+                'id' => '33',
+                'kontak_tipe_id' => '1',
+                'user_id' => '46',
+                'value' => 'https://www.facebook.com/profile.php?id=100080648587270',
+                'created_at' => '2022-08-31 17:40:20',
+                'updated_at' => '2022-08-31 17:40:20',
+            ),
+            27 => 
+            array (
+                'id' => '34',
+                'kontak_tipe_id' => '5',
+                'user_id' => '46',
+                'value' => '@ncepkazama27',
+                'created_at' => '2022-08-31 18:06:48',
+                'updated_at' => '2022-08-31 18:19:20',
+            ),
+            28 => 
+            array (
+                'id' => '35',
+                'kontak_tipe_id' => '3',
+                'user_id' => '46',
+                'value' => '085524947588',
+                'created_at' => '2022-08-31 18:09:00',
+                'updated_at' => '2022-08-31 18:09:00',
             ),
         ));
         
