6685875f9a59e8c7adf9b5778a9cac2568a6ab99
Isep Lutpi Nur <53908272+upi20@users.noreply.github.com>
2023-09-16 06:37:47 +0700

    tambah menu bawah

--- resources/views/layouts/frontend/body/footer.blade.php
+++ resources/views/layouts/frontend/body/footer.blade.php
@@ -1,3 +1,33 @@
+<nav class="navbar-bottom d-xl-none">
+    <ul class="d-flex justify-content-sm-around justify-content-between align-items-center text-center p-0 m-0">
+        <li class="{{ request()->routeIs('home') ? 'active' : '' }}">
+            <a href="{{ route('home') }}" class="nav-link" title="Halaman Utama">
+                <i class="fas fa-home"></i>
+            </a>
+        </li>
+        <li>
+            <a href="javascript:void(0)" class="nav-link" title="Pencarian" data-el-toggle=".js-search-toggle">
+                <i class="fas fa-search"></i>
+            </a>
+        </li>
+        <li class="{{ request()->routeIs('anggota') ? 'active' : '' }}">
+            <a href="{{ route('anggota') }}" class="nav-link" title="Daftar Anggota">
+                <i class="fas fa-users"></i>
+            </a>
+        </li>
+        <li class="{{ request()->routeIs('artikel') ? 'active' : '' }}">
+            <a href="{{ route('artikel') }}" class="nav-link" title="Daftar Artikel">
+                <i class="far fa-newspaper"></i>
+            </a>
+        </li>
+        <li class="{{ request()->routeIs('login') ? 'active' : '' }}">
+            <a href="{{ route('login') }}" class="nav-link" title="Login">
+                <i class="far fa-user"></i>
+            </a>
+        </li>
+    </ul>
+</nav>
+
 @php
     $anim = 1;
 @endphp
@@ -19,8 +49,8 @@
 
                             @foreach ($getSosmed_val as $sosmed)
                                 <li class="list-inline-item list-style-none"
-                                    data-anim-child="slide-left delay-{{ $anim++ }}" value="{{ $sosmed['nama'] }}"
-                                    title="{{ $sosmed['nama'] }}">
+                                    data-anim-child="slide-left delay-{{ $anim++ }}"
+                                    value="{{ $sosmed['nama'] }}" title="{{ $sosmed['nama'] }}">
                                     <a href="{{ $sosmed['url'] }}" title="{{ $sosmed['nama'] }}" target="_blank">
                                         <i class="{{ $sosmed['icon'] }}"></i>
                                     </a>
